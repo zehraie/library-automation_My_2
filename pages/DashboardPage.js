7a1db3fcbe70a5f89fa1bd98b34404bd255e1dab
zehrairkicatal <zehraie197@gmail.com>
2024-10-02 20:02:10 +0100

    logout is completed

--- pages/DashboardPage.js
+++ pages/DashboardPage.js
@@ -3,6 +3,18 @@ import { BasePage } from "./BasePage.js";
 export class DashboardPage extends BasePage {
 
   // ADD YOUR LOCATORS HERE...
+  /**
+  * @param {import('playwright').Page} page
+  */
+constructor(page) {
+ super(page);
+ this.userprofileImage = page.locator("//img[@id='user_avatar']");
+ this.logoutButton = page.getByText('Log Out');
+ //this.bookLink= page.locator("//a[@href='#books' and contains(@class, 'nav-link')]");
+ this.bookLink = page.locator("//a[@href='#books' and @class='nav-link']");
 
+
+ 
+}
 
 }
