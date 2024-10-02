899491222b265e324b5e0d9b0bc9504a902282b3
zehrairkicatal <zehraie197@gmail.com>
2024-10-02 16:24:38 +0100

    all the changes

--- utilities/BrowserUtility.js
+++ utilities/BrowserUtility.js
@@ -3,8 +3,12 @@ export class BrowserUtility {
     static async sleep(seconds){
         await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
     }
-
+// create a fuction that can verify
+//expected and actual message and return promise
     // ADD YOUR OWN BROWSER UTILITY FUNCTIONS HERE...
-    
+    static async verifyMessages(actual, expected) {
+        return new Promise((resolve) => {
+            resolve(expected === actual);
+          });
+}}
 
-}
